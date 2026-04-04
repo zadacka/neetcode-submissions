de08e58a65dc768ee60f4ce8ce7d7a9598155730
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2026-04-04 15:39:06 +0100

    Add: count-number-of-islands - submission-16

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-16.py
@@ -0,0 +1,45 @@
+class Solution:
+    def numIslands_simple_but_slow(self, grid: List[List[str]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        def search(r, c, grid):
+            if 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == '1':
+                grid[r][c] = '0'
+                search(r+1, c, grid)
+                search(r-1, c, grid)
+                search(r, c+1, grid)
+                search(r, c-1, grid)
+                return 1
+            else:
+                return 0
+        islands = 0
+        for r, row in enumerate(grid):
+            for c, _ in enumerate(row):
+                if grid[r][c] == '1':
+                    islands += search(r, c, grid)
+        
+        return islands
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        untested = set(
+            (r, c)
+            for r, row in enumerate(grid)
+            for c, _ in enumerate(row)
+            if grid[r][c] == '1'
+        )
+
+        def search(r, c):
+            if (r, c) in untested:
+                untested.remove((r, c))
+                search(r+1, c)
+                search(r-1, c)
+                search(r, c+1)
+                search(r, c-1)
+
+        islands = 0
+        while untested:
+            (r, c) = next(iter(untested)) 
+            islands += 1
+            search(r, c)
+        return islands
